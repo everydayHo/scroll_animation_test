49e0c281ee807f8e287dd5a64ec57245ebf45c2c
everydayHo <jungwoopage@gmail.com>
2023-09-30 16:19:02 +0900

    create a firework

---
+++ canvas_firework/js/Spark.js
@@ -0,0 +1,25 @@
+import CanvasOption from './CanvasOption.js';
+
+export default class Spark extends CanvasOption {
+  constructor(x, y, vx, vy, opacity, colorDeg) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.opacity = opacity;
+    this.vx = vx;
+    this.vy = vy;
+    this.colorDeg = colorDeg;
+  }
+  update() {
+    this.opacity -= 0.01;
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+    this.ctx.fillStyle = `hsla(${this.colorDeg}, 100%, 65%, ${this.opacity})`;
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+}
