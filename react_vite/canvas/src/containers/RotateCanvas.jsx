e13a33a62c5010f313652282cdd289ee25cf4b40
everydayHo <jungwoopage@gmail.com>
2024-03-24 11:49:36 +0900

    matterjs 스킬영역 생성

--- react_vite/canvas/src/containers/RotateCanvas.jsx
+++ react_vite/canvas/src/containers/RotateCanvas.jsx
@@ -1,11 +1,146 @@
+import { Engine, Render, Runner, Mouse, MouseConstraint, Composite, Bodies, Events } from 'matter-js';
+import '../style/containers/RotateCanvas.css';
+import { useEffect, useRef, useState } from 'react';
+import IconFrame from '../assets/icon_AFRAME.png';
+import IconCss from '../assets/icon_CSS.png';
+import IconHtml from '../assets/icon_HTML.png';
+import IconJs from '../assets/icon_JS.png';
+import IconReact from '../assets/icon_REACT.png';
+import IconThree from '../assets/icon_THREE.png';
+import { filter } from 'lodash';
+
+const data = {
+	JS: { title: 'Javascript', level: 4, desc: '자바스크립트에 대한 설명이라고 할 수 있습니다. 자바스크립트에 대한 설명. 자바스크립트에 대한 설명.' },
+	REACT: { title: 'React.js', level: 5, desc: 'React에 대한 설명이라고 할 수 있습니다. React에 대한 설명. React에 대한 설명.' },
+	CSS: { title: 'CSS/SASS', level: 3, desc: 'CSS에 대한 설명이라고 할 수 있습니다. CSS에 대한 설명. CSS에 대한 설명.' },
+	AFRAME: { title: 'Aframe.js', level: 4, desc: 'AFRAME에 대한 설명이라고 할 수 있습니다. AFRAME에 대한 설명. AFRAME에 대한 설명.' },
+	THREE: { title: 'Three.js', level: 2, desc: 'THREE에 대한 설명이라고 할 수 있습니다. THREE에 대한 설명. THREE에 대한 설명.' },
+	HTML: { title: 'HTML', level: 5, desc: 'HTML에 대한 설명이라고 할 수 있습니다. HTML에 대한 설명. HTML에 대한 설명.' },
+};
+
 const RotateCanvas = () => {
+	const [selected, setSelected] = useState(data['JS']);
+	const canvasRef = useRef(null);
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const cw = 1000;
+		const ch = 1000;
+		const gravityPower = 0.5;
+		let gravityDeg = 0;
+
+		let engine, render, runner, mouse, mouseConstraint, observer;
+		initScene();
+		initMouse();
+		initGround();
+		initImageBoxes();
+		initIntersectionObserver();
+		Events.on(mouseConstraint, 'mousedown', () => {
+			const newSelcted = mouseConstraint.body && data[mouseConstraint.body.label];
+			newSelcted && setSelected(newSelcted);
+		});
+		Events.on(runner, 'tick', () => {
+			gravityDeg += 1;
+			engine.world.gravity.x = gravityPower * Math.cos((Math.PI / 180) * gravityDeg);
+			engine.world.gravity.y = gravityPower * Math.sin((Math.PI / 180) * gravityDeg);
+		});
+		// canvas.addEventListener('mousewheel', () => {
+		// 	addRect(mouse.position.x, mouse.position.y, 50, 50);
+		// });
+		function initScene() {
+			engine = Engine.create();
+			render = Render.create({
+				canvas: canvas,
+				engine: engine,
+				options: {
+					width: cw,
+					height: ch,
+					wireframes: false,
+					background: '#1b1b19',
+				},
+			});
+			runner = Runner.create();
+			Render.run(render);
+			Runner.run(runner, engine);
+		}
+		function initMouse() {
+			mouse = Mouse.create(canvas);
+			mouseConstraint = MouseConstraint.create(engine, {
+				mouse: mouse,
+			});
+			Composite.add(engine.world, mouseConstraint);
+			canvas.removeEventListener('mousewheel', mouse.mousewheel);
+			canvas.removeEventListener('DOMMouseScroll', mouse.mousewheel);
+		}
+		function initIntersectionObserver() {
+			const options = {
+				rootMargin: '0px 0px 100px',
+				threshold: 0,
+			};
+			observer = new IntersectionObserver((entries) => {
+				const canvasEntry = entries[0];
+				if (canvasEntry.isIntersecting) {
+					runner.enabled = true;
+					Render.run(render);
+				} else {
+					runner.enabled = false;
+					Render.stop(render);
+				}
+			}, options);
+			observer.observe(canvas);
+		}
+		function initGround() {
+			const segments = 32;
+			const deg = (Math.PI * 2) / segments;
+			const width = 50;
+			const radius = cw / 2 + width / 2;
+			const height = radius * Math.tan(deg / 2) * 2;
+			for (let i = 0; i < segments; i++) {
+				const theta = deg * i;
+				const x = radius * Math.cos(theta) + cw / 2;
+				const y = radius * Math.sin(theta) + ch / 2;
+				addRect(x, y, width, height, { isStatic: true, angle: theta });
+			}
+		}
+		function initImageBoxes() {
+			const scale = 0.7;
+			const t1 = { w: 250 * scale, h: 250 * scale };
+			const t2 = { w: 732 * scale, h: 144 * scale };
+			addRect(cw / 2, ch / 2, t1.w, t1.h, { label: 'CSS', champer: { radius: 20 }, render: { sprite: { texture: IconCss, xScale: scale, yScale: scale } } });
+			addRect(cw / 2 - t1.w, ch / 2, t1.w, t1.h, { label: 'JS', champer: { radius: 20 }, render: { sprite: { texture: IconJs, xScale: scale, yScale: scale } } });
+			addRect(cw / 2 + t1.w, ch / 2, t1.w, t1.h, { label: 'HTML', champer: { radius: 20 }, render: { sprite: { texture: IconHtml, xScale: scale, yScale: scale } } });
+			addRect(cw / 2, ch / 2 + t1.h, t1.w, t1.h, { label: 'REACT', champer: { radius: 75 }, render: { sprite: { texture: IconReact, xScale: scale, yScale: scale } } });
+			addRect(cw / 2 - t1.w, ch / 2 + t1.h, t1.w, t1.h, { label: 'THREE', champer: { radius: 20 }, render: { sprite: { texture: IconThree, xScale: scale, yScale: scale } } });
+			addRect(cw / 2, ch / 2 - t1.h, t2.w, t2.h, { label: 'AFRAME', champer: { radius: 20 }, render: { sprite: { texture: IconFrame, xScale: scale, yScale: scale } } });
+		}
+		function addRect(x, y, w, h, options = {}) {
+			const rect = Bodies.rectangle(x, y, w, h, options);
+			Composite.add(engine.world, rect);
+		}
+		return () => {
+			// useEfffect가 언마운트 될 때 matterjs 클리어
+			observer.unobserve(canvas);
+			Composite.clear(engine.world);
+			Mouse.clearSourceEvents(mouse);
+			Runner.stop(runner);
+			Render.stop(render);
+			Engine.clear(engine);
+		};
+	}, []);
 	return (
-		<div>
-			<canvas></canvas>
+		<div className="rotate-canvas-wrapper">
+			<canvas ref={canvasRef}></canvas>
 			<aside>
-				<h1>Javascript</h1>
-				<h2>⭐️⭐️⭐️⭐️⭐️</h2>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, numquam?</p>
+				<h1>{selected.title}</h1>
+				<h2>
+					{Array(5)
+						.fill(null)
+						.map((_, i) => (
+							<span key={i} style={{ filter: `grayscale(${selected.level <= i ? 1 : 0})` }}>
+								&#11088;
+							</span>
+						))}
+				</h2>
+				<p>{selected.desc}</p>
 			</aside>
 		</div>
 	);
